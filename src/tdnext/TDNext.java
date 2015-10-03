0718abb51441c6f61ba08fffe60ab1720bea50e4
AdrielTan <a0125283@u.nus.edu>
2015-10-03 16:46:34 +0800

    Added all cases into executeCommand Created stub methods for all cases

--- src/tdnext/TDNext.java
+++ src/tdnext/TDNext.java
@@ -3,7 +3,6 @@ package tdnext;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import tdnext.ParserAPI.COMMAND_TYPE;
 import tdnext.TDNextLogicAPI.CommandType;
 
 public class TDNext {
@@ -14,18 +13,98 @@ public class TDNext {
 	}
 	
 	public ArrayList<Task> executeCommand(String input) {
-		COMMAND_TYPE command = ParserAPI.parseCommand(input);
+		CommandType command = ParserAPI.parseCommand(input);
 		
-		switch (command){
-		case ADD :  addTask(input);
-					return _listTask;
-		case DELETE : 
-						break;
+		switch (command) {
+			case ADD :  
+				addTask(input);
+				sortDefault();
+				return _listTask;
+				
+			case DELETE : 
+				deleteTask(input);
+				sortDefault();
+				return _listTask;
+			
+			case SEARCH :
+				ArrayList<Task> output = searchTask(input);
+				return output;
+			
+			case EDIT :
+				editTask(input);
+				sortDefault();
+				return _listTask;
+				
+			case CLEAR :
+				clearAll();
+				return _listTask;
+			
+			case DONE :
+				markTaskAsDone(input);
+				return _listTask;
+				
+			case SORT_DEFAULT :
+				sortDefault();
+				return _listTask;
+			
+			case SORT_BY_NAME :
+				sortName();
+				return _listTask;
+				
+			case SORT_BY_DEADLINE :
+				sortDeadline();
+				return _listTask;
+				
+			case UNDO:
+				undo();
+				return _listTask;
+				
+			case EXIT :
+				exitProgram();
+				return _listTask;
+			
+			default :
+				return _listTask;
 		}
-		
-		return _listTask;
 	}
 	
+
+
+	private void undo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void editTask(String input) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void clearAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void markTaskAsDone(String input) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void sortName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void sortDeadline() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void exitProgram() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	private void addTask(String input) {
 		ArrayList<String> information = ParserAPI.parseInformation(input);
 		Task newTask = new Task(information);
@@ -38,18 +117,15 @@ public class TDNext {
 		}
 	}
 	
-	/*private void deleteTask(String input) {
-		ArrayList<String> information = ParserAPI.parseInformation(input);
-		int index = searchTask(information.get(0));
-		Task removedTask = _listTask.remove(index);
-		StorageAPI.deleteFromFile(removedTask.toString());
+	private void deleteTask(String input) {
 	}
 	
-	private int searchTask(String name) {
-		for(int i = 0; i < _listTask.size(); i++) {
-			if(_listTask.get(i).toString().equals(name)) {
-				return i;
-			}
-		}
-	}*/
+	private ArrayList<Task> searchTask(String name) {
+		return new ArrayList<Task>(); 
+	}
+	
+	private void sortDefault() {
+		// TODO Auto-generated method stub
+		
+	}
 }
