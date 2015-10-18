4d6ba8660948b1d545de0a5c6b479cc9765ed91d
AdrielTan <a0125283@u.nus.edu>
2015-10-18 21:36:14 +0800

    added static string messages added testDelete method

--- src/tdnext/TestLogic.java
+++ src/tdnext/TestLogic.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 
 public class TestLogic {
 	private static String EMPTY_STRING = "";
+	private static String ADD_TASK1 = "ADD task1";
+	private static String ADD_TASK2 = "ADD task2";
+	private static String ADD_TASK3_WITHDATE = "ADD task3 BY 30/10/2015";
+	private static String DELETE_INDEX1 = "DELETE 1";
 	
 	TDNextLogicAPI _testLogic = new TDNextLogicAPI();
 	ArrayList<Task> _output = new ArrayList<Task>();
@@ -20,8 +24,7 @@ public class TestLogic {
 	@Test
 	public void testAddWithTwoSimpleTask() {
 		ArrayList<String> allInputs = new ArrayList<String>();
-		String input1 = "ADD task1";
-		allInputs.add(input1);
+		allInputs.add(ADD_TASK1);
 		
 		try {
 			for(int i = 0; i < allInputs.size(); i++){
@@ -41,10 +44,8 @@ public class TestLogic {
 	@Test
 	public void testAddWithAnotherSimpleTask() {
 		ArrayList<String> allInputs = new ArrayList<String>();
-		String input1 = "ADD task1";
-		allInputs.add(input1);
-		String input2 = "ADD task2";
-		allInputs.add(input2);
+		allInputs.add(ADD_TASK1);
+		allInputs.add(ADD_TASK2);
 		
 		try {
 			for(int i = 0; i < allInputs.size(); i++){
@@ -64,18 +65,38 @@ public class TestLogic {
 	@Test
 	public void testAddWithTaskWithDate() {
 		ArrayList<String> allInputs = new ArrayList<String>();
-		String input1 = "ADD task1";
-		allInputs.add(input1);
-		String input2 = "ADD task2";
-		allInputs.add(input2);
-		String input3 = "ADD task3 BY 30/10/2015";
-		allInputs.add(input3);
+		allInputs.add(ADD_TASK1);
+		allInputs.add(ADD_TASK2);
+		allInputs.add(ADD_TASK3_WITHDATE);
 		
 		try {
 			for(int i = 0; i < allInputs.size(); i++){
 				_output = _testLogic.executeCommand(allInputs.get(i));
 			}
 			String desiredOutput = "task3 BY 30/10/2015\n" + "task1\n" + "task2\n";
+			String testOutput = new String();
+			for(int i = 0; i < _output.size(); i++) {
+				testOutput = testOutput + _output.get(i).toString() + "\n";
+			}
+			assertEquals(EMPTY_STRING, desiredOutput, testOutput);
+		} catch (Exception e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testDelete() {
+		ArrayList<String> allInputs = new ArrayList<String>();
+		allInputs.add(ADD_TASK1);
+		allInputs.add(ADD_TASK2);
+		allInputs.add(ADD_TASK3_WITHDATE);
+		allInputs.add(DELETE_INDEX1);
+		
+		try {
+			for(int i = 0; i < allInputs.size(); i++){
+				_output = _testLogic.executeCommand(allInputs.get(i));
+			}
+			String desiredOutput = "task1\n" + "task2\n";
 			String testOutput = new String();
 			for(int i = 0; i < _output.size(); i++) {
 				testOutput = testOutput + _output.get(i).toString() + "\n";
