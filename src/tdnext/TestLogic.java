331708b5b3ce8395b9f034861c422aac6aa54a24
AdrielTan <a0125283@u.nus.edu>
2015-10-18 21:23:29 +0800

    added 3 test cases for add command

--- src/tdnext/TestLogic.java
+++ src/tdnext/TestLogic.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class TestLogic {
+	private static String EMPTY_STRING = "";
+	
 	TDNextLogicAPI _testLogic = new TDNextLogicAPI();
 	ArrayList<Task> _output = new ArrayList<Task>();
 
@@ -16,16 +18,69 @@ public class TestLogic {
 	}
 	
 	@Test
-	public void testAddWithSimpleTask() {
-		String input = "ADD task1";
+	public void testAddWithTwoSimpleTask() {
+		ArrayList<String> allInputs = new ArrayList<String>();
+		String input1 = "ADD task1";
+		allInputs.add(input1);
+		
 		try {
-			_output = _testLogic.executeCommand(input);
+			for(int i = 0; i < allInputs.size(); i++){
+				_output = _testLogic.executeCommand(allInputs.get(i));
+			}
 			String desiredOutput = "task1\n";
 			String testOutput = new String();
 			for(int i = 0; i < _output.size(); i++) {
 				testOutput = testOutput + _output.get(i).toString() + "\n";
 			}
-			assertEquals("", desiredOutput, testOutput);
+			assertEquals(EMPTY_STRING, desiredOutput, testOutput);
+		} catch (Exception e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testAddWithAnotherSimpleTask() {
+		ArrayList<String> allInputs = new ArrayList<String>();
+		String input1 = "ADD task1";
+		allInputs.add(input1);
+		String input2 = "ADD task2";
+		allInputs.add(input2);
+		
+		try {
+			for(int i = 0; i < allInputs.size(); i++){
+				_output = _testLogic.executeCommand(allInputs.get(i));
+			}
+			String desiredOutput = "task1\n" + "task2\n";
+			String testOutput = new String();
+			for(int i = 0; i < _output.size(); i++) {
+				testOutput = testOutput + _output.get(i).toString() + "\n";
+			}
+			assertEquals(EMPTY_STRING, desiredOutput, testOutput);
+		} catch (Exception e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testAddWithTaskWithDate() {
+		ArrayList<String> allInputs = new ArrayList<String>();
+		String input1 = "ADD task1";
+		allInputs.add(input1);
+		String input2 = "ADD task2";
+		allInputs.add(input2);
+		String input3 = "ADD task3 BY 30/10/2015";
+		allInputs.add(input3);
+		
+		try {
+			for(int i = 0; i < allInputs.size(); i++){
+				_output = _testLogic.executeCommand(allInputs.get(i));
+			}
+			String desiredOutput = "task3 BY 30/10/2015\n" + "task1\n" + "task2\n";
+			String testOutput = new String();
+			for(int i = 0; i < _output.size(); i++) {
+				testOutput = testOutput + _output.get(i).toString() + "\n";
+			}
+			assertEquals(EMPTY_STRING, desiredOutput, testOutput);
 		} catch (Exception e) {
 			fail();
 		}
