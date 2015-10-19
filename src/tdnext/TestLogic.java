eb6bfcc66cb69b95a039359062ae28786a301387
AdrielTan <a0125283@u.nus.edu>
2015-10-19 09:46:33 +0800

    added testSearch

--- src/tdnext/TestLogic.java
+++ src/tdnext/TestLogic.java
@@ -11,10 +11,12 @@ public class TestLogic {
 	private static String ADD_TASK1 = "ADD task1";
 	private static String ADD_TASK2 = "ADD task2";
 	private static String ADD_TASK3_WITHDATE = "ADD task3 BY 30/10/2015";
+	private static String ADD_HOMEWORK1 = "ADD homework1";
 	private static String DELETE_INDEX1 = "DELETE 1";
 	private static String DELETE_INDEXOUTOFBOUND = "DELETE 100";
 	private static String EDIT_INDEX1 = "EDIT 1 taskEdit";
 	private static String EDIT_INDEXOUTOFBOUND = "EDIT 100 taskEdit";
+	private static String SEARCH_HOME = "SEARCH home";
 	
 	TDNextLogicAPI _testLogic = new TDNextLogicAPI();
 	ArrayList<Task> _output = new ArrayList<Task>();
@@ -168,6 +170,30 @@ public class TestLogic {
 			String desiredOutput = "Index: 99, Size: 2";
 			String testOutput = e.getMessage();
 			assertEquals(EMPTY_STRING, desiredOutput, testOutput);
+		}
+	}
+	
+	@Test
+	public void testSearch() {
+		ArrayList<String> allInputs = new ArrayList<String>();
+		allInputs.add(ADD_TASK1);
+		allInputs.add(ADD_TASK2);
+		allInputs.add(ADD_TASK3_WITHDATE);
+		allInputs.add(ADD_HOMEWORK1);
+		allInputs.add(SEARCH_HOME);
+		
+		try {
+			for(int i = 0; i < allInputs.size(); i++){
+				_output = _testLogic.executeCommand(allInputs.get(i));
+			}
+			String desiredOutput = "homework1\n";
+			String testOutput = new String();
+			for(int i = 0; i < _output.size(); i++) {
+				testOutput = testOutput + _output.get(i).toString() + "\n";
+			}
+			assertEquals(EMPTY_STRING, desiredOutput, testOutput);
+		} catch (Exception e) {
+			fail();
 		}
 	}
 }
